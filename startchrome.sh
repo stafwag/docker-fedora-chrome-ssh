1ee1f47733cd38e467750bca7c3619ba24af4488
Staf Wagemakers <staf@wagemakers.be>
2015-05-12 11:17:16 +0200

    sudo removed

--- startchrome.sh
+++ startchrome.sh
@@ -1,5 +1,5 @@
-sudo docker run --privileged -d -p 127.0.0.1:8022:22 --name "mychrome" stafwag/docker-fedora-chrome-ssh
+docker run --privileged -d -p 127.0.0.1:8022:22 --name "mychrome" stafwag/docker-fedora-chrome-ssh
 sleep 1
 ssh -X mychrome google-chrome --no-default-browser-check
-sudo docker kill samwag_mychrome
-sudo docker rm samwag_mychrome
+docker kill mychrome
+docker rm mychrome
